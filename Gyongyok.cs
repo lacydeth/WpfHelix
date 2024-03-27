572b8860973f608298d6734699ec2c3da373a103
bankuti.laszlo <bankuti.laszlo@58-18>
2024-03-27 12:01:29 +0100

    Add project files.

---
+++ Gyongyok.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHelixToolkit
+{
+    internal class Gyongyok
+    {
+        int x, y, z, ertek;
+
+        public Gyongyok(string sor)
+        {
+            string[] mezok = sor.Split(';');
+            this.x = int.Parse(mezok[0]);
+            this.y = int.Parse(mezok[1]);
+            this.z = int.Parse(mezok[2]);
+            this.ertek = int.Parse(mezok[3]);
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Z { get => z; set => z = value; }
+        public int Ertek { get => ertek; set => ertek = value; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using HelixToolkit.Wpf;
+
+namespace WpfHelixToolkit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Gyongyok> gyongyok = new();
+
+            foreach (string sor in File.ReadAllLines("gyongyok.txt").Skip(1))
+            {
+                gyongyok.Add(new Gyongyok(sor));
+            }
+            
+            foreach (Gyongyok aktBogyo in gyongyok)
+            {
+                EllipsoidVisual3D ellipsoid = new EllipsoidVisual3D();
+                ellipsoid.Center = new Point3D(aktBogyo.X, aktBogyo.Y, aktBogyo.Z);
+                ellipsoid.Fill = new SolidColorBrush(Colors.LightGreen);
+                ter.Children.Add(ellipsoid);
+            }
+        }
+    }
+}
